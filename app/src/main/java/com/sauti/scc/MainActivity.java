acc428e1ce84bf78d6468c5e63bafbe1eed983e8
joshua <javanmega0@gmail.com>
2021-02-23 06:25:44 +0300

    third commit

--- app/src/main/java/com/sauti/scc/MainActivity.java
+++ app/src/main/java/com/sauti/scc/MainActivity.java
@@ -16,98 +16,41 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private CoordinatorLayout mDrawerLayout;
-    private NavigationView nav_view;
-    private ActionBarDrawerToggle mActionBarDrawerToggle;
+private BottomNavigationView bottomview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDrawerLayout = findViewById(R.id.drawerLayout);
-        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
-        mActionBarDrawerToggle.syncState();
+
+        bottomview = findViewById(R.id.navigation);
+        bottomview.setItemIconTintList(null);
 
         ActionBar actionBar = getSupportActionBar();
         if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
             getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.green)));
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setTitle("SCC");
 
         }
-        nav_view = findViewById(R.id.nav_view);
 
-        if (nav_view != null) {
-            setupDrawerContent(nav_view);
-            nav_view.setItemIconTintList(null);
-        }
     }
-        private void setupDrawerContent ( final NavigationView navigationView){
-            navigationView.getMenu().getItem(0).setChecked(true);
-            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()) {
-
-                        case R.id.app1:
-                            System.out.println("Move to app");
-                            String url = "https://play.google.com/store/apps/details?id=com.wallpapers_8k_Anime_Lofi.app";
-                            Intent rateIntent = new Intent(Intent.ACTION_VIEW);
-                            rateIntent.setData(Uri.parse(url));
-                            startActivity(rateIntent);
-                            break;
-
-                        case R.id.rate:
-                            System.out.println("Move to app");
-                            String url5 = "https://play.google.com/store/apps/details?id=com.ohrnin.hashtags4gains";
-                            Intent rateIntent5 = new Intent(Intent.ACTION_VIEW);
-                            rateIntent5.setData(Uri.parse(url5));
-                            startActivity(rateIntent5);
-                            break;
-                        case R.id.email:
-                            System.out.println("Move to app");
-                            String url6 = "https://sauti.cc";
-                            Intent rateIntent6 = new Intent(Intent.ACTION_VIEW);
-                            rateIntent6.setData(Uri.parse(url6));
-                            startActivity(rateIntent6);
-                            break;
-
-
-//                            Intent intent = new Intent(getApplicationContext(), Admin.class);
-//                            startActivity(intent);
-                    }
-                    item.setChecked(true);
-                    mDrawerLayout.closeDrawers();
-                    return true;
-                }
-            });
-        }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer_view, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected (@NonNull MenuItem item){
-        int id = item.getItemId();
-        if (id == R.id.notifications) {
-            return true;
-        }
-
-        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onResume () {
